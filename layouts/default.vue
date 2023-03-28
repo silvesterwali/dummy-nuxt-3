d5647086a787fdc31463bd760349a932091e66d1
Silvester Wali <silvesterlhwali123@gmail.com>
2023-03-28 17:29:33 +0800

    remove image

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,10 @@
           <div class="flex justify-start lg:w-0 lg:flex-1 ml-2 sm:ml-0">
             <NuxtLink to="/">
               <span class="sr-only">Rental</span>
-              <img class="h-8 w-auto sm:h-10" src="~/assets/images/kresna-logo.png" alt="logo rental">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+</svg>
+
             </NuxtLink>
           </div>
           <div>
